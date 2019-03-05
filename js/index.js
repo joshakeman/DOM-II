aa1944615c09111e353da4c94232ed7ddbd5a4f5
Josh Akeman <joshakeman@gmail.com>
2019-03-05 16:53:32 -0500

    finally figured out what was wrong

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+function myFunction() {
+    button.style.background = 'red';
+    console.log('test');
+  }
+  
+let button = document.querySelector('.btn');
+button.addEventListener("mouseover", myFunction);
+  

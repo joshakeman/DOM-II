8b8865b6198aff0b440cf6f61188045fd9fae15f
Josh Akeman <joshakeman@gmail.com>
2019-03-05 17:17:31 -0500

    Added first two events

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 // Your code goes here
+
+//mouseover
 function myFunction() {
     button.style.background = 'red';
-    console.log('test');
   }
   
 let button = document.querySelector('.btn');
 button.addEventListener("mouseover", myFunction);
+
+// wheel
+
+function myFunction2() {
+  p = document.querySelectorAll('p');
+  p = Array.from(p);
+  p.forEach( function (e) {
+    e.style.fontSize = '20px';
+  });
+}
   
+let body = document.querySelector('body');
+body.addEventListener("wheel", myFunction2);
+
